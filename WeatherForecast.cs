fb2276e51c39ad96e2b531fd794e75b26641f4ae
Jose Balado <JoseBalado@users.noreply.github.com>
2020-02-10 16:44:08 +0000

    Initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace webapi
+{
+    public class WeatherForecast
+    {
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+    }
+}
