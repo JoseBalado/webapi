2d1a071ed36aec03c0ba39661736b5eb098564a6
Jose Balado <JoseBalado@users.noreply.github.com>
2020-02-11 15:56:04 +0000

    Persist List<users>

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -13,13 +13,13 @@ namespace webapi.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-         private List<User> users = new List<User>
-            {
-                new User { id = 1, name="Vithal Wadje",email="Mumbai@gmail.com",password="99999999999" },
-                new User { id = 2, name = "Sudhir Wadje", email = "Latur@gmail.com", password = "88888888888888888888" },
-                new User { id = 3, name = "Anil", email = "Delhi@gmail.com", password = "77777777777777777777" },
-                new User { id = 4, name = "Jose", email = "jose@gmail.com", password = "password" }
-            };
+         static volatile List<User> users = new List<User>
+         {
+              new User { id = 1, name="Vithal Wadje",email="Mumbai@gmail.com",password="99999999999" },
+              new User { id = 2, name = "Sudhir Wadje", email = "Latur@gmail.com", password = "88888888888888888888" },
+              new User { id = 3, name = "Anil", email = "Delhi@gmail.com", password = "77777777777777777777" },
+              new User { id = 4, name = "Jose", email = "jose@gmail.com", password = "password" }
+         };
 
         [HttpGet]
         [Route("[action]/{id}")]
