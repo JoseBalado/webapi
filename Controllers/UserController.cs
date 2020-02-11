30a7f45839e6cb595c0f4bed0911e9e0e6cfa662
Jose Balado <JoseBalado@users.noreply.github.com>
2020-02-11 14:10:55 +0000

    Move List<User> list outside the method

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,6 +12,14 @@ namespace webapi.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
+         private List<User> users = new List<User>
+            {
+                new User { id = 1, name="Vithal Wadje",email="Mumbai@gmail.com",password="99999999999" },
+                new User { id = 2, name = "Sudhir Wadje", email = "Latur@gmail.com", password = "88888888888888888888" },
+                new User { id = 3, name = "Anil", email = "Delhi@gmail.com", password = "77777777777777777777" },
+                new User { id = 4, name = "Jose", email = "jose@gmail.com", password = "password" }
+            };
+
         [HttpGet]
         [Route("[action]/{id}")]
         public User getUser(int id)
@@ -33,14 +41,6 @@ namespace webapi.Controllers
         public List<User> getAllUsers()
         {
             Console.WriteLine("Get all users");
-
-            var users = new List<User>
-            {
-                new User { id = 1, name="Vithal Wadje",email="Mumbai@gmail.com",password="99999999999" },
-                new User { id = 2, name = "Sudhir Wadje", email = "Latur@gmail.com", password = "88888888888888888888" },
-                new User { id = 3, name = "Anil", email = "Delhi@gmail.com", password = "77777777777777777777" },
-                new User { id = 4, name = "Jose", email = "jose@gmail.com", password = "password" }
-            };
 
             return users;
         }
