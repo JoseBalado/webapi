b2cc344c14b0b68e4a7bbba484fdfed098ffc505
Jose Balado <JoseBalado@users.noreply.github.com>
2020-02-11 12:25:20 +0000

    Return a list of users

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -15,7 +15,7 @@ namespace webapi.Controllers
         [Route("[action]/{id}")]
         public User getUser(int id)
         {
-            Console.WriteLine("User number: " + id);
+            Console.WriteLine("Get user number: " + id);
             var user = new User
             {
                 name = "Jose",
@@ -24,6 +24,23 @@ namespace webapi.Controllers
             };
 
             return user;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public List<User> getAllUsers()
+        {
+            Console.WriteLine("Get all users");
+
+            var users = new List<User>
+            {
+                new User { name="Vithal Wadje",email="Mumbai@gmail.com",password="99999999999" },
+                new User { name = "Sudhir Wadje", email = "Latur@gmail.com", password = "88888888888888888888" },
+                new User { name = "Anil", email = "Delhi@gmail.com", password = "77777777777777777777" },
+                new User { name = "Jose", email = "jose@gmail.com", password = "password" }
+            };
+
+            return users;
         }
     }
 }
